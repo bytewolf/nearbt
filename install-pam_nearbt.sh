99f15838e55861a8af671c5cf83018695ca1b7f8
guoc <guochen42@gmail.com>
2016-03-17 19:43:35 +1100

    Package liboath

--- install-pam_nearbt.sh
+++ install-pam_nearbt.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
+LIBOATH_SOURCE_PATH="/usr/local/lib/liboath.dylib"
+LIBOATH_LOCAL_PATH="./pam_nearbt/liboath.dylib"
+LIBOATH_INSTALL_PATH="/usr/local/lib/pam_nearbt/liboath.dylib"
+
+if [ ! -e "./pam_nearbt/liboath.dylib" ]
+then
+    cp "$LIBOATH_SOURCE_PATH" "$LIBOATH_LOCAL_PATH"
+fi
+
+sudo install_name_tool -id "$LIBOATH_INSTALL_PATH" "$LIBOATH_LOCAL_PATH"
+
+sudo cp "$LIBOATH_LOCAL_PATH" "$LIBOATH_INSTALL_PATH"
+
 sudo xcodebuild clean build install -project pam_nearbt.xcodeproj -target pam_nearbt -configuration Release DSTROOT=/

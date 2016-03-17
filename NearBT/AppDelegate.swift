1be909358569c6bebd745f676536de358bdcea52
guoc <guochen42@gmail.com>
2016-03-17 23:39:09 +1100

    Add local notification when application will terminate (did not test)

--- NearBT/AppDelegate.swift
+++ NearBT/AppDelegate.swift
@@ -52,12 +52,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate, PeripheralControllerDeleg
     }
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        let userNotificationSettings = UIUserNotificationSettings(forTypes: [.Alert, .Badge, .Sound], categories: nil)
+        UIApplication.sharedApplication().registerUserNotificationSettings(userNotificationSettings)
         peripheralController = PeripheralController(delegate: self)
         peripheralController.start()
         return true
     }
-
-    
 
     func applicationDidBecomeActive(application: UIApplication) {
         if let viewController = window?.rootViewController as? ViewController {
@@ -65,5 +65,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate, PeripheralControllerDeleg
         }
     }
 
+    func applicationWillTerminate(application: UIApplication) {
+        let notification: UILocalNotification = {
+            let notification = UILocalNotification()
+            notification.alertTitle = "NearBT will terminate"
+            notification.alertBody = "Relaunch to keep it working in background."
+            return notification
+        }()
+        UIApplication.sharedApplication().presentLocalNotificationNow(notification)
+    }
     
 }

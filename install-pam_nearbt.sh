a17d8f27793957192c37aa646cf6245c7a48af6b
guoc <guochen42@gmail.com>
2016-03-28 23:58:08 +1100

    Change install-pam_nearbt.sh to Makefile for easy prefix installation. Prepare for homebrew.

--- install-pam_nearbt.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-sudo xcodebuild clean build install -project pam_nearbt.xcodeproj -target pam_nearbt -configuration Release DSTROOT=/usr/local
-sudo xcodebuild clean build install -project pam_nearbt.xcodeproj -target pam_nearbt-setup -configuration Release DSTROOT=/usr/local
